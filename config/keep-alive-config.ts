6de99a7be78242048e6caa50ac4c47a9344051cd
Utkarsh Verma <108287933+Utkarshvr@users.noreply.github.com>
2025-04-07 22:11:31 +0530

    removed otherEndpoints from config

--- config/keep-alive-config.ts
+++ config/keep-alive-config.ts
@@ -1,13 +1,11 @@
-export type KeepAliveConfig = typeof keepAliveConfig
-
+export type KeepAliveConfig = typeof keepAliveConfig;
 
 export const keepAliveConfig = {
-
   // Select a table in your Supabase database to make a call to
-  table: 'keep-alive',
+  table: "keep-alive",
 
   // Column that will be queried with a random string
-  column: 'name',
+  column: "name",
 
   // Configuration for actions taken on the database
   allowInsertionAndDeletion: true, // Set this to false unless you're using a 'keep-alive'-dedicated table
@@ -16,8 +14,5 @@ export const keepAliveConfig = {
 
   consoleLogOnError: true,
 
-  otherEndpoints: [
-    'https://your-other-vercel-project-urls.vercel.app/api/keep-alive',
-    'https://your-other-supabase-app.com/api/keep-alive',
-  ]
-}
+  otherEndpoints: [],
+};
